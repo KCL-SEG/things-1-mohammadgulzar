cce45dbb47cd9f9a31e0866c8e59eff9d91b5a59
Mohammadgulzar Habibullah <mohammedgulzar79@gmail.com>
2023-10-01 20:07:52 +0100

    Complete Exercise 3:Refining the Thing model

--- things/models.py
+++ things/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
+def valid_quantity(value):
+    if value < 0 or value > 100:
+        raise ValueError(f"{value} is not a valid quantity")
+
+
 class Thing(models.Model):
-    name = models.CharField()
-    description = models.CharField()
-    quantity = models.IntegerField()
+    name = models.CharField(unique=True, blank=False, max_length=30)
+    description = models.CharField(unique=False, blank=True, max_length=120)
+    quantity = models.IntegerField(unique=False, validators=[valid_quantity])
